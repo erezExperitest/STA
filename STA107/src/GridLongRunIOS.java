90787df8ab3d19587d1ea4fd933b521fc8cbe15f
erez.akri <erez.akri@erezakri.experitest.com>
2017-03-30 12:55:06 +0300

    GridLongRunIOS

--- STA107/src/GridLongRunIOS.java
+++ STA107/src/GridLongRunIOS.java
@@ -4,27 +4,25 @@ import org.junit.*;
 /**
  *
  */
-public class versioninstall {
-    private String host = "localhost";
-    private int port = 8889;
-    private String projectBaseDirectory = "/Users/erez.akri/workspace/project4";
+public class GridLongRunIOS {
+
+    private String projectBaseDirectory = "C:\\Users\\erez.akri.experitest\\workspace\\project31";
     protected Client client = null;
 
     @Before
     public void setUp(){
-        client = new Client(host, port, true);
-        client.setProjectBaseDirectory(projectBaseDirectory);
-        client.setReporter("xml", "reports", "versioninstall");
     }
 
     @Test
-    public void testversioninstall(){
-        client.setDevice("ios_app:Pink iPhone");
+    public void GridLongRun(){
+        GridClient grid = new GridClient("admin", "Experitest2012", "", "http://localhost"); //(user name, password, project,ip,port,secured)
+        client = grid.lockDeviceForExecution("erezTest", "@os='ios'", 10, 3000000); //(test name, query, time for reserve in minuets, timeout in milliseconds)
+        client.setProjectBaseDirectory(projectBaseDirectory);
+        client.setReporter("xml", "reports", "1");
+
         client.launch("com.experitest.ExperiBank", true, true);
         client.click("NATIVE", "placeholder=Username", 0, 1);
-        if(client.waitForElement("NATIVE", "placeholder=Username", 0, 30000)){
-            // If statement
-        }
+        client.waitForElement("NATIVE", "placeholder=Username", 0, 30000);
         client.elementSendText("NATIVE", "placeholder=Username", 0, "company");
         client.elementSendText("NATIVE", "placeholder=Password", 0, "company");
         client.click("NATIVE", "accessibilityLabel=loginButton", 0, 1);
@@ -47,53 +45,29 @@ public class versioninstall {
         client.sendText("{ENTER}");
         client.click("WEB", "text=Images", 0, 1);
         client.click("WEB", "name=1tqX0q1YsulfuM:", 0, 1);
-        if(client.waitForElement("NATIVE", "xpath=//*[@class='UIAWebView']", 0, 10000)){
-            // If statement
-        }
+        client.waitForElement("NATIVE", "xpath=//*[@class='UIAWebView']", 0, 10000);
         client.elementSendText("NATIVE", "xpath=//*[@class='UIAWebView']", 0, "google.co.il");
         client.sendText("{ENTER}");
         client.elementSendText("NATIVE", "xpath=//*[@class='UIAWebView']", 0, "wikipedia.com");
         client.sendText("{ENTER}");
         client.elementSendText("WEB", "id=searchInput", 0, "pele");
-        if(client.waitForElement("WEB", "text=Brazilian footballer", 0, 30000)){
-            // If statement
-        }
+        client.waitForElement("WEB", "text=Brazilian footballer", 0, 30000);
         client.click("WEB", "text=Brazilian footballer", 0, 1);
-        if(client.waitForElement("WEB", "id=searchIcon", 0, 10000)){
-            // If statement
-        }
+        client.waitForElement("WEB", "id=searchIcon", 0, 10000);
         client.click("WEB", "id=searchIcon", 0, 1);
-        if(client.waitForElement("WEB", "name=search", 0, 10000)){
-            // If statement
-        }
+        client.waitForElement("WEB", "name=search", 0, 10000);
         client.click("WEB", "name=search", 0, 1);
         client.elementSendText("WEB", "name=search", 0, "expert");
         client.sendText("{ENTER}");
         client.click("WEB", "id=searchInput", 0, 1);
-        if(client.waitForElement("WEB", "name=search", 0, 30000)){
-            // If statement
-        }
+        client.waitForElement("WEB", "name=search", 0, 30000);
         client.click("WEB", "name=search", 0, 1);
-        if(client.waitForElement("WEB", "text=Close", 0, 10000)){
-            // If statement
-        }
+        client.waitForElement("WEB", "text=Close", 0, 10000);
         client.click("WEB", "text=Close", 0, 1);
         client.elementSendText("WEB", "id=searchInput", 0, "jerusalem");
-        if(client.waitForElement("WEB", "text=
-
-                Jerusalem
-                ancient holy city in the Judean Mountains
-                ", 0, 30000)){
-                // If statement
-    }
-        client.click("WEB", "text=
-
-    Jerusalem
-    ancient holy city in the Judean Mountains
-  ", 0, 1);
-          if(client.waitForElement("WEB", "text=Kingdom of Judah", 0, 60000)){
-        // If statement
-    }
+        client.waitForElement("WEB", "text=Jerusalemancient holy city in the Judean Mountains", 0, 30000);
+        client.click("WEB", "text=Jerusalemancient holy city in the Judean Mountains", 0, 1);
+        client.waitForElement("WEB", "text=Kingdom of Judah", 0, 60000);
         client.click("WEB", "text=Kingdom of Judah", 0, 1);
 }
 
